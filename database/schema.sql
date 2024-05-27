8584425d400876d8a428c1e7bba99b093db08bcf
S-P-R <spr2354@gmail.com>
2024-05-27 12:16:52 -0700

    Create date_summary view
    The date_summary view is intended to simplify the GET date API route

--- database/schema.sql
+++ database/schema.sql
@@ -1,17 +1,26 @@
 CREATE SCHEMA news_headlines;
 
-create table news_headlines.source (
+CREATE TABLE news_headlines.source (
     name VARCHAR(50) PRIMARY KEY, 
     link VARCHAR(50)
 );
 
-create table news_headlines.headline (
+CREATE TABLE news_headlines.headline (
     text VARCHAR (100) NOT NULL,
     date DATE NOT NULL, 
     sentiment DECIMAL CHECK (sentiment >= -1 AND sentiment <= 1), 
     source VARCHAR(50) references news_headlines.source(name),
     PRIMARY KEY (text, date)
 );
+
+create view date_summary AS
+SELECT 
+    date,
+    AVG(sentiment) AS avg_sentiment
+FROM 
+    headline
+GROUP BY 
+    date;
 
 CREATE INDEX headline_date_idx ON news_headlines.headline (date);
 CREATE INDEX headline_date_source_idx ON news_headlines.headline (date, source);
